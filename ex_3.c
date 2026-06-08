9a1e4c7f28680e7c3890977b970a8a985bab2958
ateitelbaum6 <ateitelbaum6@gmail.com>
2026-06-08 22:52:38 +0300

    Update ex_3.c

--- ex_3.c
+++ ex_3.c
@@ -276,13 +276,12 @@ void printSudoku(int board[SUDOKU_GRID_SIZE][SUDOKU_GRID_SIZE])
 void task1ReversePhraseImplementation(){
   char ch;
   scanf("%c ", &ch);
-  printf("reversed phrase is", %d);
+  printf("reversed phrase is", "%d");
   if (ch == '\n') {
     return;
   }
   task1ReversePhraseImplementation();
   printf("%c", ch);
-}
 }
 
 
